a5f91bfd5b8cfed57e815a7cfc9530d71b5baf95
hexaalif <shadullahsad9@gmail.com>
2025-01-25 22:22:54 +0600

    zoom functionality added

--- src/components/Posts/ImgLightBox.tsx
+++ src/components/Posts/ImgLightBox.tsx
@@ -1,6 +1,7 @@
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/captions.css";
 
 interface Media {
   type: string;
@@ -21,6 +22,7 @@ const ImgLightBox = ({ open, onClose, media }: lightboxProps) => {
         open={open}
         close={onClose}
         slides={media.map((item) => ({ src: item.src }))}
+        zoom={{ maxZoomPixelRatio: 3, scrollToZoom: true }}
       />
     </div>
   );
